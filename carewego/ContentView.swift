8105551fee2a41ad2ea5da88eee3e805f192d392
pimoux <luka.vegeta@laposte.net>
2021-12-08 11:21:02 +0100

    First commit

---
+++ carewego/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  carewego
+//
+//  Created by Apprenant 60 on 08/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ carewego/carewegoApp.swift
@@ -0,0 +1,17 @@
+//
+//  carewegoApp.swift
+//  carewego
+//
+//  Created by Apprenant 60 on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct carewegoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
