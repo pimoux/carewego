841432cab8b7e600823e7f59e7d06e17334ccf1b
pimoux <luka.vegeta@laposte.net>
2021-12-09 16:45:57 +0100

    Database updated

--- carewego/Models/Structures.swift
+++ carewego/Models/Structures.swift
@@ -8,31 +8,36 @@
 import Foundation
 
 struct User: Identifiable {
+    static let users = [user1, user2, user3, user4, user5, user6, user7]
     var id = UUID()
     var username: String
     var avatar: String
     var isProfessionnal: Bool
-    var handicaps: [Handicap]
+    var handicaps: [Handicap]?
     var savedPosts: [Post]?
     var savedUsers: [User]?
 }
 
 struct Comment: Identifiable {
+    static let comments = [comment1, comment2, comment3, comment4]
     var id = UUID()
     var user: User
     var content: String
 }
 
 struct Post: Identifiable {
+    static let posts = [post1, post2, post3, post4, post5]
     var id = UUID()
     var user: User
     var content: String
     var images: [String]?
+    var createdAt: Date
     var likes: Int
     var comments: Int
 }
 
 struct Location: Identifiable {
+    static let locations = [location1, location2, location3, location4, location5, location6]
     var id = UUID()
     var type: SearchLocationType
     var name: String
@@ -40,10 +45,12 @@ struct Location: Identifiable {
 }
 
 struct Service: Identifiable {
+    static let services = [service1, service2, service3, service4, service5, service6]
     var id = UUID()
     var name: String
     var images: [String]
     var location: Location
+    var type: SearchServiceType
     var horaires: String
     var abilities: [Handicap]
     var rate: Double
